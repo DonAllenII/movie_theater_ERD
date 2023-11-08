81ea181e1ac7d4786eb1c865392cd036519b6980
DonAllenII <don.a.alexanderii@gmail.com>
2023-11-07 22:38:32 -0500

    added concessions insertions

---
+++ concessions_insertions.sql
@@ -0,0 +1,11 @@
+insert into concessions(food_id, food_name, food_type, price)
+	values
+	(1, 'Dornish Wine', 'alcohol', 12.99),
+	(2, 'Sour Goat Milk', 'alcohol', 5.99),
+	(3, 'Ale', 'alcohol', 7.99),
+	(4, 'Pomegranate Juice', 'drink', 6.99),
+	(5, 'Bread', 'food', 5.99),
+	(6, 'Hard Cheese', 'food', 8.99),
+	(7, 'Figs', 'food', 9.99),
+	(8, 'Lemon Cakes', 'food', 7.99)
+	;
